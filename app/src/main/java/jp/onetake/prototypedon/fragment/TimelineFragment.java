1289e33e3a26cebca2ffb9a452ebea2a082d4458
Shizuo Ichitake <developer.tk74@gmail.com>
2017-06-11 23:24:04 +0900

    タイムラインの改良
    * Pull to Refreshの追加

--- app/src/main/java/jp/onetake/prototypedon/fragment/TimelineFragment.java
+++ app/src/main/java/jp/onetake/prototypedon/fragment/TimelineFragment.java
@@ -2,6 +2,7 @@ package jp.onetake.prototypedon.fragment;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,9 +21,16 @@ import jp.onetake.prototypedon.mastodon.TimelineType;
 import jp.onetake.prototypedon.text.StatusLinkMovementMethod;
 import jp.onetake.prototypedon.widget.TimelineAdapter;
 
-public class TimelineFragment extends BasicFragment implements ApiExecuteThread.ApiResultListener, StatusLinkMovementMethod.LinkClickListener {
+public class TimelineFragment extends BasicFragment
+		implements ApiExecuteThread.ApiResultListener, StatusLinkMovementMethod.LinkClickListener {
 	public interface TimelineEventListener {
 		void onLinkClick(Uri uri);
+	}
+
+	private enum Trigger {
+		Initial,
+		Refresh,
+		Add,
 	}
 
 	private static final String KEY_INSTANCE	= "TimelineFragment.KEY_INSTANCE";
@@ -38,16 +46,26 @@ public class TimelineFragment extends BasicFragment implements ApiExecuteThread.
 		return fragment;
 	}
 
+	private SwipeRefreshLayout mRefreshLayout;
 	private ListView mListView;
 	private View mProgressView;
 	private Instance mInstance;
 	private TimelineType mType;
+	private Trigger mTrigger;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 
 		View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_timeline, container, false);
+
+		mRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.layout_refresh);
+		mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				launchRequest(Trigger.Refresh);
+			}
+		});
 
 		StatusLinkMovementMethod method = new StatusLinkMovementMethod();
 		method.setListener(this);
@@ -65,15 +83,19 @@ public class TimelineFragment extends BasicFragment implements ApiExecuteThread.
 		super.onActivityCreated(savedInstanceState);
 
 		mInstance = getArguments().getParcelable(KEY_INSTANCE);
-
 		mType = TimelineType.getByString(getArguments().getString(KEY_TYPE));
-		TimelinesRequest request = new TimelinesRequest(getContext(), mType);
 
-		mProgressView.setVisibility(View.VISIBLE);
+		launchRequest(Trigger.Initial);
+	}
+
+	private void launchRequest(Trigger trigger) {
+		TimelinesRequest request = new TimelinesRequest(getContext(), mType);
 
 		ApiExecuteThread thread = ApiExecuteThread.newInstance(mInstance, request);
 		thread.setListener(this);
 		thread.start();
+
+		showProgress(trigger);
 	}
 
 	@Override
@@ -81,16 +103,21 @@ public class TimelineFragment extends BasicFragment implements ApiExecuteThread.
 		TimelinesResponse res = (TimelinesResponse)response;
 
 		TimelineAdapter adapter = (TimelineAdapter)mListView.getAdapter();
+
+		if (mTrigger == Trigger.Refresh) {
+			adapter.clear();
+		}
+
 		adapter.add(res.statusList);
 		adapter.notifyDataSetChanged();
 
-		mProgressView.setVisibility(View.INVISIBLE);
+		dismissProgress();
 	}
 
 	@Override
 	public void onApiFailure(int identifier, ApiException exception) {
 		if (identifier == getResources().getInteger(R.integer.api_id_public_timeline)) {
-			mProgressView.setVisibility(View.INVISIBLE);
+			dismissProgress();
 
 			Toast.makeText(getContext(), R.string.message_get_timeline_error, Toast.LENGTH_LONG).show();
 		}
@@ -100,6 +127,33 @@ public class TimelineFragment extends BasicFragment implements ApiExecuteThread.
 	public void onClick(TextView textView, Uri uri) {
 		if (getActivity() instanceof TimelineEventListener) {
 			((TimelineEventListener)getActivity()).onLinkClick(uri);
+		}
+	}
+
+	private void showProgress(Trigger trigger) {
+		switch (trigger) {
+			case Initial:
+				mProgressView.setVisibility(View.VISIBLE);
+				break;
+			case Add:
+				// TODO:追加読み込みのために出すぐるぐるを表示する
+				break;
+		}
+
+		mTrigger = trigger;
+	}
+
+	private void dismissProgress() {
+		switch (mTrigger) {
+			case Initial:
+				mProgressView.setVisibility(View.INVISIBLE);
+				break;
+			case Refresh:
+				mRefreshLayout.setRefreshing(false);
+				break;
+			case Add:
+				// TODO:追加読み込みのために出すぐるぐるを消す
+				break;
 		}
 	}
 }
